a01f5ac99e5979251f4fcf1c1fe66df7c9c1562d
ThomasWoods <thomaspwoods@gmail.com>
2019-11-08 14:56:10 -0800

    Game and MainMenu

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Game : MonoBehaviour
+{
+	public AudioMixer audioMixer;
+	public GameObject audioPlayerPrefab;
+	[HideInInspector]
+	public AudioSource BGMPlayer, SFXPlayer;
+	public float MasterVolume {
+		get { float volume = 0.5f; audioMixer.GetFloat("MasterVolume",out volume); return volume; }
+		set { audioMixer.SetFloat("MasterVolume", value); } }
+	public float BGMVolume {
+		get { float volume = 0.5f; audioMixer.GetFloat("BGMVolume", out volume); return volume; }
+		set { audioMixer.SetFloat("BGMVolume", value); } }
+	public float SFXVolume {
+		get { float volume = 0.5f; audioMixer.GetFloat("SFXVolume", out volume); return volume; }
+		set { audioMixer.SetFloat("SFXVolume", value); } }
+
+	public static Game _instance;
+	public static Game instance
+	{
+		get {
+			if (_instance) return _instance;
+			else {
+				Game findGame = FindObjectOfType<Game>();
+				if (findGame != null) return findGame;
+				else {
+					GameObject newGameObj = new GameObject("Game");
+					findGame = newGameObj.AddComponent<Game>();
+					return findGame;
+				}
+			}
+		}
+		set {
+			if (_instance != null) Destroy(_instance.gameObject);
+			_instance = value;
+		}
+	}
+	
+    void Awake()
+    {
+
+		if (audioMixer == null) audioMixer = Resources.Load("GameAudioMixer") as AudioMixer;
+		if (audioPlayerPrefab == null) audioPlayerPrefab = Resources.Load("Prefabs/AudioPlayer") as GameObject;
+		if (audioPlayerPrefab == null) { Destroy(gameObject); Debug.Log("No AudioPlayer resource!"); return; }
+
+		GameObject newObj = Instantiate(audioPlayerPrefab);
+		BGMPlayer = newObj.GetComponent<AudioSource>();
+		newObj = Instantiate(audioPlayerPrefab);
+		SFXPlayer = newObj.GetComponent<AudioSource>();
+
+		DontDestroyOnLoad(gameObject);
+
+		LoadSettings();
+
+		instance = this;
+
+	}
+
+	void LoadSettings()
+	{
+		if (PlayerPrefs.HasKey("MasterVolume")) MasterVolume = PlayerPrefs.GetFloat("MasterVolume");
+		if (PlayerPrefs.HasKey("BGMVolume")) MasterVolume = PlayerPrefs.GetFloat("BGMVolume");
+		if (PlayerPrefs.HasKey("SFXVolume")) MasterVolume = PlayerPrefs.GetFloat("SFXVolume");
+	}
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+	public Slider MasterVolumeSlider, BGMSlider, SFXSlider;
+
+	public void ChangeMasterVolume(float f) { Game.instance.MasterVolume = MasterVolumeSlider.value; }
+	public void ChangeBGMVolume(float f) { Game.instance.BGMVolume = BGMSlider.value; }
+	public void ChangeSFXVolume(float f) { Game.instance.SFXVolume = SFXSlider.value; }
+
+	void OnEnable()
+	{
+		InitializeVolumeSliders();
+	}
+
+	void InitializeVolumeSliders()
+	{
+		if (MasterVolumeSlider != null)
+		{
+			MasterVolumeSlider.value = Game.instance.MasterVolume;
+			MasterVolumeSlider.onValueChanged.AddListener(ChangeMasterVolume);
+		}
+		if (BGMSlider != null)
+		{
+			BGMSlider.value = Game.instance.BGMVolume;
+			MasterVolumeSlider.onValueChanged.AddListener(ChangeMasterVolume);
+		}
+		if (SFXSlider != null)
+		{
+			SFXSlider.value = Game.instance.SFXVolume;
+			MasterVolumeSlider.onValueChanged.AddListener(ChangeMasterVolume);
+		}
+	}
+}
