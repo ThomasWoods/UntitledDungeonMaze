712078f310a4dbf618a9fc971d62144f8063bbe7
ThomasWoods <thomaspwoods@gmail.com>
2019-11-08 22:21:30 -0800

    Fixed bug in settings

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,9 +7,9 @@ public class MenuController : MonoBehaviour
 {
 	public Slider MasterVolumeSlider, BGMSlider, SFXSlider;
 
-	public void ChangeMasterVolume(float f) { Game.instance.MasterVolume = MasterVolumeSlider.value; }
-	public void ChangeBGMVolume(float f) { Game.instance.BGMVolume = BGMSlider.value; }
-	public void ChangeSFXVolume(float f) { Game.instance.SFXVolume = SFXSlider.value; }
+	public void ChangeMasterVolume(float f) { Game.instance.MasterVolume = f; }
+	public void ChangeBGMVolume(float f) { Game.instance.BGMVolume = f; }
+	public void ChangeSFXVolume(float f) { Game.instance.SFXVolume = f; }
 
 	void OnEnable()
 	{
@@ -26,12 +26,12 @@ public class MenuController : MonoBehaviour
 		if (BGMSlider != null)
 		{
 			BGMSlider.value = Game.instance.BGMVolume;
-			MasterVolumeSlider.onValueChanged.AddListener(ChangeMasterVolume);
+			BGMSlider.onValueChanged.AddListener(ChangeBGMVolume);
 		}
 		if (SFXSlider != null)
 		{
 			SFXSlider.value = Game.instance.SFXVolume;
-			MasterVolumeSlider.onValueChanged.AddListener(ChangeMasterVolume);
+			SFXSlider.onValueChanged.AddListener(ChangeSFXVolume);
 		}
 	}
 }
