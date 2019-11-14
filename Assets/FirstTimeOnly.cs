581c91509c975a6549daeb07dca1683562c51fe8
ThomasWoods <thomaspwoods@gmail.com>
2019-11-14 13:39:12 -0800

    Added first time message to help the player get started

---
+++ Assets/FirstTimeOnly.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FirstTimeOnly : MonoBehaviour
+{
+	public static bool Ran;
+	public UnityEvent onFirstAwake = new UnityEvent();
+	public UnityEvent onSubsequentAwakes = new UnityEvent();
+
+	void Awake()
+	{
+		if (!FirstTimeOnly.Ran)
+		{
+			Debug.Log("First Time Run");
+			onFirstAwake.Invoke();
+			Ran = true;
+		}
+		else
+		{
+			Debug.Log("onSubsequentAwakes");
+			onSubsequentAwakes.Invoke();
+		}
+	}
+}
